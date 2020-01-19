8d632b0667e01a38fc2a0077bb0723171657ab2f
David B <d.jbradley.ii@gmail.com>
2020-01-19 03:32:07 -0800

    Adds auth router to app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const teamsRouter = require('./teams/teams-router');
 const itemsRouter = require('./items/items-router');
@@ -21,6 +22,7 @@ app.use(cors());
 
 /* ROUTES */
 
+app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/teams', teamsRouter);
 app.use('/api/items', itemsRouter);
