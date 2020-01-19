6fdccbe182355a744250d1206c50ce4cb2b66226
David B <d.jbradley.ii@gmail.com>
2020-01-18 21:57:23 -0800

    Adds insert user service along with logic for password validation

--- users/users-service.js
+++ users/users-service.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
+
 const UsersService = {
   getAllUsers(knex) {
     return knex.select(
@@ -5,7 +8,6 @@ const UsersService = {
       'fname',
       'lname',
       'platform',
-      'email',
       'gamertag',
       'rocket_id',
       'rank',
@@ -21,7 +23,6 @@ const UsersService = {
         'fname',
         'lname',
         'platform',
-        'email',
         'gamertag',
         'rocket_id',
         'rank',
@@ -32,6 +33,49 @@ const UsersService = {
       .from('users')
       .where('id', userid)
       .first();
+  },
+  insertUser(knex, newUser) {
+    return knex
+      .insert(newUser)
+      .into('users')
+      .returning([
+        'id',
+        'fname',
+        'lname',
+        'platform',
+        'gamertag',
+        'rocket_id',
+        'rank',
+        'division',
+        'lft',
+        'bio'
+      ])
+      .then(([user]) => user);
+  },
+  validatePassword(password) {
+    // password length
+    if (password.length < 8 || password.length > 72) {
+      return 'Password must be between 8 and 72 characters';
+    }
+
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces';
+    }
+
+    // password contains digit, using a regex here
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain 1 upper case, lower case, number and special character (!@#$%^&)';
+    }
+    return null;
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  hasUserWithEmail(db, email) {
+    return db('users')
+      .where({ email })
+      .first()
+      .then((user) => !!user);
   }
 };
 
