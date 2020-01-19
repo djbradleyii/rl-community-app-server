955348f4a458abf8786930e804f6140c6ca217d9
David B <d.jbradley.ii@gmail.com>
2020-01-19 03:09:29 -0800

    Removes serialize function and adds requireAuth middleware

--- src/teams/teams-router.js
+++ src/teams/teams-router.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const TeamsService = require('./teams-services');
 const teamsRouter = express.Router();
-const xss = require('xss');
-
-const serializeUser = (user) => ({
-  id: user.id,
-  fname: xss(user.fname), 
-  lname: xss(user.lname), 
-  platform: xss(user.platform), 
-  gamertag: xss(user.gamertag), 
-  rocket_id: xss(user.rocket_id), 
-  rank: xss(user.rank), 
-  division: xss(user.division), 
-  lft: user.lft, 
-  email: xss(user.email),
-  password: xss(user.password), 
-  bio: xss(user.bio),
-  date_created: new Date(user.date_created),
-});
+const { requireAuth } = require('../middleware/jwt-auth');
 
 teamsRouter.route('/')
-.get((req, res, next) => {
+.get(requireAuth, (req, res, next) => {
     TeamsService.getAllUsersLookingForTeam(req.app.get('db'))
     .then((users) => {
       res.json(users);
