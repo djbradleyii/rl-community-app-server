eb2df4457ff24a4ec398af55caa0d5812e13d479
David B <d.jbradley.ii@gmail.com>
2020-01-24 00:11:26 -0800

    Updates CLIENT_ORIGIN domain for cors

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    //CLIENT_ORIGIN: 'https://rl-community-app.now.sh',
-    //CLIENT_ORIGIN: 'http://localhost:3000',
+    CLIENT_ORIGIN: 'https://rocket-league-community-app.now.sh/',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dbradley:password@localhost/rl-community-app',
     TEST_DATABASE_URL: 'postgresql://dbradley:password@localhost/rl-community-app-test',
     JWT_SECRET: process.env.JWT_SECRET || 'superdupersecretword',
